fb7fa17c06db7ed966eb6ab7cb45e4f461eb55fc
iJaddu <Computers99!!>
2020-12-03 20:34:55 -0500

    Added museum prices

--- app/src/main/java/com/example/cs213_project5/TicketActivity.java
+++ app/src/main/java/com/example/cs213_project5/TicketActivity.java
@@ -22,15 +22,45 @@ public class TicketActivity extends AppCompatActivity {
         textView.setText(message);
 
         ImageView museumName = (ImageView)findViewById(R.id.museumImage);
+        TextView adultAdmission = (TextView)findViewById(R.id.adultPrice);
+        TextView seniorAdmission = (TextView)findViewById(R.id.seniorPrice);
+        TextView studentAdmission = (TextView)findViewById(R.id.studentPrice);
+        int adultCost = 0;
+        int seniorCost = 0;
+        int studentCost = 0;
 
         if(message.equals("The Museum of Modern Art")){
             museumName.setImageResource(R.drawable.moma);
+            adultAdmission.setText("Adult $25");
+            seniorAdmission.setText("Senior $18");
+            studentAdmission.setText("Student $14");
+            adultCost = 25;
+            seniorCost = 18;
+            studentCost = 14;
         }else if(message.equals("The Museum of Natural History")){
             museumName.setImageResource(R.drawable.natural_history);
+            adultAdmission.setText("Adult $23");
+            seniorAdmission.setText("Senior $18");
+            studentAdmission.setText("Student $18");
+            adultCost = 23;
+            seniorCost = 18;
+            studentCost = 18;
         }else if(message.equals("The Metropolitan Museum of Art")){
             museumName.setImageResource(R.drawable.met);
+            adultAdmission.setText("Adult $25");
+            seniorAdmission.setText("Senior $17");
+            studentAdmission.setText("Student $12");
+            adultCost = 25;
+            seniorCost = 17;
+            studentCost = 12;
         }else if(message.equals("The Brooklyn Museum")){
             museumName.setImageResource(R.drawable.brooklyn);
+            adultAdmission.setText("Adult $16");
+            seniorAdmission.setText("Senior $10");
+            studentAdmission.setText("Student $10");
+            adultCost = 16;
+            seniorCost = 10;
+            studentCost = 10;
         }
     }
 
