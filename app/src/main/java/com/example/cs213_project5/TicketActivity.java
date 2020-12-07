0c8668f7b259d4f29f81a378948aae8918124c42
NightOnTheSun <Backpacking#420>
2020-12-07 00:16:01 -0500

    Handles clickable image and links to the corresponding museum website Added ticketPrice, SaleTax, and total textviews

--- app/src/main/java/com/example/cs213_project5/TicketActivity.java
+++ app/src/main/java/com/example/cs213_project5/TicketActivity.java
@@ -1,7 +1,12 @@
 package com.example.cs213_project5;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,15 +26,17 @@ public class TicketActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.MuseumName);
         textView.setText(message);
 
-        ImageView museumName = (ImageView)findViewById(R.id.museumImage);
-        TextView adultAdmission = (TextView)findViewById(R.id.adultPrice);
-        TextView seniorAdmission = (TextView)findViewById(R.id.seniorPrice);
-        TextView studentAdmission = (TextView)findViewById(R.id.studentPrice);
+        ImageView museumName = (ImageView) findViewById(R.id.museumImage);
+        TextView adultAdmission = (TextView) findViewById(R.id.adultPrice);
+        TextView seniorAdmission = (TextView) findViewById(R.id.seniorPrice);
+        TextView studentAdmission = (TextView) findViewById(R.id.studentPrice);
+
+
         int adultCost = 0;
         int seniorCost = 0;
         int studentCost = 0;
 
-        if(message.equals("The Museum of Modern Art")){
+        if (message.equals("The Museum of Modern Art")) {
             museumName.setImageResource(R.drawable.moma);
             adultAdmission.setText("Adult $25");
             seniorAdmission.setText("Senior $18");
@@ -37,7 +44,9 @@ public class TicketActivity extends AppCompatActivity {
             adultCost = 25;
             seniorCost = 18;
             studentCost = 14;
-        }else if(message.equals("The Museum of Natural History")){
+
+
+        } else if (message.equals("The Museum of Natural History")) {
             museumName.setImageResource(R.drawable.natural_history);
             adultAdmission.setText("Adult $23");
             seniorAdmission.setText("Senior $18");
@@ -45,7 +54,7 @@ public class TicketActivity extends AppCompatActivity {
             adultCost = 23;
             seniorCost = 18;
             studentCost = 18;
-        }else if(message.equals("The Metropolitan Museum of Art")){
+        } else if (message.equals("The Metropolitan Museum of Art")) {
             museumName.setImageResource(R.drawable.met);
             adultAdmission.setText("Adult $25");
             seniorAdmission.setText("Senior $17");
@@ -53,7 +62,7 @@ public class TicketActivity extends AppCompatActivity {
             adultCost = 25;
             seniorCost = 17;
             studentCost = 12;
-        }else if(message.equals("The Brooklyn Museum")){
+        } else if (message.equals("The Brooklyn Museum")) {
             museumName.setImageResource(R.drawable.brooklyn);
             adultAdmission.setText("Adult $16");
             seniorAdmission.setText("Senior $10");
@@ -62,7 +71,72 @@ public class TicketActivity extends AppCompatActivity {
             seniorCost = 10;
             studentCost = 10;
         }
+
+
+
     }
+
+
+
+
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+
+    /**
+     * Handles clickable Image and links to the associated museums website
+     * @param view
+     */
+
+    public void museumImageOnCLick(View view) {
+
+        Intent intent = getIntent();
+        String museumId = intent.getStringExtra("MUSEUM_ID");
+
+
+        if (museumId.equals("The Museum of Modern Art")) {
+
+            Uri website = Uri.parse("https://www.moma.org/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, website);
+            startActivity(launchBrowser);
+
+
+        } else if (museumId.equals("The Museum of Natural History")) {
+
+            Uri website = Uri.parse("https://www.amnh.org/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, website);
+            startActivity(launchBrowser);
+
+        } else if (museumId.equals("The Metropolitan Museum of Art")) {
+
+            Uri website = Uri.parse("https://www.metmuseum.org/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, website);
+            startActivity(launchBrowser);
+
+        } else if (museumId.equals("The Brooklyn Museum")) {
+
+            Uri website = Uri.parse("https://www.brooklynmuseum.org/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, website);
+            startActivity(launchBrowser);
+
+        }
+
+    }
+
 
 
 }
