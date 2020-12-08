2b24993f169244c55297cc3f5dd9f314e0097025
NightOnTheSun <Backpacking#420>
2020-12-08 01:06:49 -0500

    Handles clickable image and links to the corresponding museum website Added ticketPrice, SaleTax, and total textviews

--- app/src/main/java/com/example/cs213_project5/TicketActivity.java
+++ app/src/main/java/com/example/cs213_project5/TicketActivity.java
@@ -11,7 +11,16 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TicketActivity extends AppCompatActivity {
+public class TicketActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+
+    double total =0.0;
+    int adultCost = 0;
+    int seniorCost = 0;
+    int studentCost = 0;
+    int numberTickets=0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +40,10 @@ public class TicketActivity extends AppCompatActivity {
         TextView seniorAdmission = (TextView) findViewById(R.id.seniorPrice);
         TextView studentAdmission = (TextView) findViewById(R.id.studentPrice);
 
+        Spinner adultTickets = (Spinner)findViewById(R.id.adultTickets);
+        Spinner seniorTickets = (Spinner)findViewById(R.id.seniorTickets);
+        Spinner studentTickets = (Spinner)findViewById(R.id.studentTickets);
 
-        int adultCost = 0;
-        int seniorCost = 0;
-        int studentCost = 0;
 
         if (message.equals("The Museum of Modern Art")) {
             museumName.setImageResource(R.drawable.moma);
@@ -46,6 +55,8 @@ public class TicketActivity extends AppCompatActivity {
             studentCost = 14;
 
 
+
+
         } else if (message.equals("The Museum of Natural History")) {
             museumName.setImageResource(R.drawable.natural_history);
             adultAdmission.setText("Adult $23");
@@ -54,6 +65,9 @@ public class TicketActivity extends AppCompatActivity {
             adultCost = 23;
             seniorCost = 18;
             studentCost = 18;
+
+
+
         } else if (message.equals("The Metropolitan Museum of Art")) {
             museumName.setImageResource(R.drawable.met);
             adultAdmission.setText("Adult $25");
@@ -62,6 +76,10 @@ public class TicketActivity extends AppCompatActivity {
             adultCost = 25;
             seniorCost = 17;
             studentCost = 12;
+
+
+
+
         } else if (message.equals("The Brooklyn Museum")) {
             museumName.setImageResource(R.drawable.brooklyn);
             adultAdmission.setText("Adult $16");
@@ -70,7 +88,32 @@ public class TicketActivity extends AppCompatActivity {
             adultCost = 16;
             seniorCost = 10;
             studentCost = 10;
+
+
+
+
         }
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tickets, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adultTickets.setAdapter(adapter);
+        adultTickets.setOnItemSelectedListener(this);
+
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.tickets, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+
+        seniorTickets.setAdapter(adapter2);
+        seniorTickets.setOnItemSelectedListener(this);
+
+
+
+        studentTickets.setAdapter(adapter);
+        studentTickets.setOnItemSelectedListener(this);
+
 
 
 
@@ -78,22 +121,28 @@ public class TicketActivity extends AppCompatActivity {
 
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        TextView ticketCostView = (TextView)findViewById(R.id.ticketCostView);
+        TextView salesTaxView = (TextView)findViewById(R.id.salesTaxView);
+        TextView totalPriceView = (TextView)findViewById(R.id.totalView);
+        double salesTax = 0.0, ticketCost = 0.0;
+
+        numberTickets = position;
+        total = numberTickets * adultCost;
+
+        System.out.println("checking->>:" + id);
+        totalPriceView.setText(String.valueOf(total));
 
 
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
-        switch (position) {
-            case 0:
-                // Whatever you want to happen when the first item gets selected
-                break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
-                break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
-                break;
 
-        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
 
@@ -136,7 +185,5 @@ public class TicketActivity extends AppCompatActivity {
         }
 
     }
-
-
 
 }
